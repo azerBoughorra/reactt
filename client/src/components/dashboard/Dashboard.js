ed4d5e2c1e0ca5b3a6876d0af141931b131433c5
Azer Boughorra <azer.boughorra@gmail.com>
2020-03-05 02:02:50 +0100

    reservation / cancel reservation + redux

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,49 +1,57 @@
 
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Card from 'react-bootstrap/Card'
 import axios from "axios";
-import { useDispatch } from 'react-redux';
-import { reserveHouseAction } from "../../redux-actions/guest-house.actions";
+import { useDispatch, useSelector } from 'react-redux';
+import { reserveHouseAction, cancelReserveHouseAction } from "../../redux-actions/guest-house.actions";
+import { selectReservedHouses } from "../../redux-selectors/guest-house.selectors";
 
-class Dashboard extends Component {
-  dispatch = useDispatch();
+const Dashboard = () => {
+  const dispatch = useDispatch();
 
-  state = {
-    houses: []
-  }
-
-  findGuestHouse() {
+  const [houses, setHouses] = useState(null);
+  const reservation = useSelector(selectReservedHouses)
+  if (!houses) {
     axios
       .get("/api/house/findAll")
-      .then(res => { this.setState({ houses: res.data }) })
+      .then(res => {
+        setHouses(res.data);
+        console.log('res', res);
+      })
   }
 
 
-  render() {
-    this.findGuestHouse();
-    return (
-      <div className="container">
-        {this.state.houses.length > 0 ?
-          this.state.houses.map(house =>
-            <Card className="bg-dark text-white">
-              <Card.Img src={house.image} alt="Card image" className="card-img" />
-              <Card.ImgOverlay>
-                <Card.Title>{house.name}</Card.Title>
-                <Card.Text>{house.region}</Card.Text>
-                <Card.Text>
-                  {house.description}
-                </Card.Text>
-                <Card.Text>{house.rating}</Card.Text>
-                <Card.Footer>
-                <button variant="primary" onClick={this.dispatch(reserveHouseAction(house))}>Go somewhere</button>
-                </Card.Footer>
-              </Card.ImgOverlay>
-            </Card>
-          ) : null
-        }
-      </div>
-    )
-  }
+
+
+
+  return (
+    <div className="container">
+      {houses && houses.length > 0 ?
+        houses.map(house =>
+          <Card className="bg-dark text-white">
+            <Card.Img src={house.image} alt="Card image" className="card-img" />
+            <Card.ImgOverlay>
+              <Card.Title>{house.name}</Card.Title>
+              <Card.Text>{house.region}</Card.Text>
+              <Card.Text>
+                {house.description}
+              </Card.Text>
+              <Card.Text>{house.rating}</Card.Text>
+              <Card.Footer>
+                {
+                  reservation.find(h => h._id == house._id) ?
+                    <button variant="primary" onClick={() => dispatch(cancelReserveHouseAction(house._id))}>Annuler la reservation</button>
+                    :
+                    <button variant="primary" onClick={() => dispatch(reserveHouseAction(house))}>Reserver</button>
+
+                }
+              </Card.Footer>
+            </Card.ImgOverlay>
+          </Card>
+        ) : null
+      }
+    </div>
+  )
 }
 
 export default Dashboard;
