c7217335af8cad66d3a872b2945e78b7387d8844
unknown <skander.jenhani@gmail.com>
2020-03-05 02:24:33 +0100

    Dashboard cards Filter
    Filter par région et par Nom

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,18 @@
+import axios from "axios";
 
 import React, { Component } from "react";
 import Card from 'react-bootstrap/Card'
-import axios from "axios";
 import { useDispatch } from 'react-redux';
 import { reserveHouseAction } from "../../redux-actions/guest-house.actions";
+import { Container, Row, Col } from 'react-bootstrap'
 
 class Dashboard extends Component {
-  dispatch = useDispatch();
+  // dispatch = useDispatch();
 
   state = {
-    houses: []
+    houses: [],
+    searchByName: '',
+    searchByRegion: ''
   }
 
   findGuestHouse() {
@@ -18,27 +21,47 @@ class Dashboard extends Component {
       .then(res => { this.setState({ houses: res.data }) })
   }
 
+  updateSearchRegion(event) {
+    this.setState({ searchByRegion: event.target.value.substr(0, 20) });
+  }
+
+  updateSearchName(event) {
+    this.setState({ searchByName: event.target.value.substr(0, 20) });
+  }
 
   render() {
     this.findGuestHouse();
+    let filteredhouses = this.state.houses.filter(e => {
+      return e.region.toLowerCase().indexOf(this.state.searchByRegion.toLowerCase()) !== -1 && e.name.toLowerCase().indexOf(this.state.searchByName.toLowerCase()) !== -1
+    })
     return (
       <div className="container">
+        <Row>
+          <Col sm="6"> Filtrer par région : <input type="text" placeholder="Région" value={this.state.searchByRegion} onChange={this.updateSearchRegion.bind(this)} /></Col>
+          <Col sm="6"> Filtrer par Nom : <input type="text" placeholder="Nom" value={this.state.searchByName} onChange={this.updateSearchName.bind(this)} /></Col>
+        </Row>
         {this.state.houses.length > 0 ?
-          this.state.houses.map(house =>
-            <Card className="bg-dark text-white">
-              <Card.Img src={house.image} alt="Card image" className="card-img" />
-              <Card.ImgOverlay>
-                <Card.Title>{house.name}</Card.Title>
-                <Card.Text>{house.region}</Card.Text>
-                <Card.Text>
-                  {house.description}
-                </Card.Text>
-                <Card.Text>{house.rating}</Card.Text>
-                <Card.Footer>
-                <button variant="primary" onClick={this.dispatch(reserveHouseAction(house))}>Go somewhere</button>
-                </Card.Footer>
-              </Card.ImgOverlay>
-            </Card>
+          filteredhouses.map(house =>
+            <Container fluid>
+              <Row>
+                <Col sm="4">
+                  <Card className="bg-dark text-white">
+                    <Card.Img src={house.image} alt="Card image" />
+                    <Card.ImgOverlay>
+                      <Card.Title>{house.name}</Card.Title>
+                      <Card.Text>{house.region}</Card.Text>
+                      <Card.Text>
+                        {house.description}
+                      </Card.Text>
+                      <Card.Text>{house.rating}</Card.Text>
+                      <Card.Footer>
+                        <button variant="primary"> Go somewhere</button>
+                      </Card.Footer>
+                    </Card.ImgOverlay>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
           ) : null
         }
       </div>
